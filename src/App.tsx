6b7b7baa551b804074c4bf3e8b2faaad28ffb4ef
Lelik Jan <80705108+Lelik7777@users.noreply.github.com>
2021-10-27 09:26:56 +0300

    add router-react-dom

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import o from './App.module.css';
 import {Header} from './components/header/Header';
 import {Nav} from './components/nav/Nav';
 import {Profile} from './components/content/profile/Profile';
+import {Dialogs} from './components/content/dialog/Dialogs';
 
 function App() {
     return (
         <div className={o.AppWrapper}>
             <Header/>
             <Nav/>
-            <Profile/>
-
+            <div className={o.content}>
+             {/*   <Profile/>*/}
+                <Dialogs/>
+            </div>
         </div>
     );
 }
---
+++ src/components/content/dialog/Dialogs.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import o from './Dialogs.module.css';
+
+export function Dialogs() {
+    return (
+        <div className={o.dialogs}>
+dialogs
+        </div>
+    )
+}
